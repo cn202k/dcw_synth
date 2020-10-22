aaf1d022d367275628a1674e5ce3f18e45e70c01
cn202k <cn202k@gmail.com>
2020-10-22 15:19:46 +0900

    fix typo

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 pip install -q kaggle
-mkdir ./kaggle
-cp kaggle.json ./kaggle/
-chmod 600 ./kaggle/kaggle.json
+mkdir ./.kaggle
+cp kaggle.json ./.kaggle/
+chmod 600 ./.kaggle/kaggle.json
 kaggle datasets download -d andrewmvd/animal-faces
 unzip -qq animal-faces.zip
 mv ./afhq ./dataset
